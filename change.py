639844fded2296ac08f346f679390b83673f0ed1
MontovioI <ilmontovio@mail.austral.edu.ar>
2025-03-31 12:16:02 -0300

    Update change.py

--- change.py
+++ change.py
@@ -2,6 +2,6 @@ def change():
     expense = 23.75
     money = 100
     print(f"Ingresar gasto:\n{expense}\nDinero recibido\n{money}\n")
-    resto=float(money)-float(expense)
+    resto=money-expense
     decimal=int((resto-int(resto))*100)
     print(f"Vuelto\n\nPesos:\n{int(resto)}\nCentavos:\n{decimal}")

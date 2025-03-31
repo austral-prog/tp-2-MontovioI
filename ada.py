58e42eb18f878999ab169c938549aa4c4ba96bc8
MontovioI <ilmontovio@mail.austral.edu.ar>
2025-03-31 11:45:28 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,9 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    ncompleto= f"{first_name} {last_name}"
+    #Resolución
+    print(ncompleto.lower())
+    print(ncompleto.title())
+    print(ncompleto.upper())
+    print(f"\t{ncompleto.lower()}")
